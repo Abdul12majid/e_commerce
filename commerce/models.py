7772f4f0511d27fad8faa705261ca6fdc7c039a9
Abdul12majid <yisaabdulmajid@gmail.com>
2023-10-17 06:07:28 +0100

    Data Structure, Models and Rendering.

--- commerce/models.py
+++ commerce/models.py
@@ -26,10 +26,11 @@ class Product(models.Model):
 			url = self.image.url
 		except:
 			url = ''
+		return url
 	
 
 class Order(models.Model):
-	Customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
 	date_ordered = models.DateTimeField(auto_now_add=True)
 	complete = models.BooleanField(default=False, blank=False, null=True)
 	transaction_id = models.CharField(max_length=200, null=True)
@@ -37,11 +38,28 @@ class Order(models.Model):
 	def __str__(self):
 		return str(self.id)
 
+	@property
+	def get_cart_total(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.get_total for item in orderitems ])
+		return total
+
+	@property
+	def get_cart_items(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.quantity for item in orderitems ])
+		return total
+
 class OrderItem(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
 	Order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
+
+	@property
+	def get_total(self):
+		total = self.product.price * self.quantity
+		return total
 
 class Shipping_Address(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
--- commerce/views.py
+++ commerce/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
-	return render(request, 'commerce/home.html')
+	products = Product.objects.all()
+	context={'products':products}
+	return render(request, 'commerce/home.html', context)
 
 def cart(request):
-	return render(request, 'commerce/cart.html')
+	if request.user.is_authenticated:
+		customer=request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items= order.orderitem_set.all()
+	else:
+		items=[]
+		order = {'get_cart_total':0, 'get_cart_items':0}
+
+	context={'items':items, 'order':order}
+	return render(request, 'commerce/cart.html', context)
 
 def checkout(request):
-	return render(request, 'commerce/checkout.html')
+	if request.user.is_authenticated:
+		customer=request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items= order.orderitem_set.all()
+	else:
+		items=[]
+		order = {'get_cart_total':0, 'get_cart_items':0}
+
+	context={'items':items, 'order':order}
+	return render(request, 'commerce/checkout.html', context)
